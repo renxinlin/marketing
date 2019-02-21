5eff82523cb808c7467e3caa2f7efde94c6ff18f
zhuchuang <zhuchuang@app315.net>
2019-02-21 17:03:22 +0800

    修改端口

--- src/main/java/com/jgw/supercodeplatform/marketing/controller/MarketingActivityController.java
+++ src/main/java/com/jgw/supercodeplatform/marketing/controller/MarketingActivityController.java
@@ -13,8 +13,8 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.Map;
 
 @RestController
-@RequestMapping("/marketing/members")
-public class MarketingMembersController {
+@RequestMapping("/marketing/activity")
+public class MarketingActivityController {
 
 
     @RequestMapping(value = "",method = RequestMethod.POST)
