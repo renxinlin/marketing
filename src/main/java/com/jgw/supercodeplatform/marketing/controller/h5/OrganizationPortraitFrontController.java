cd697b56f6b5a4917ceecc320d9dde61d69402f9
zhuchuang <zhuchuang@app315.net>
2019-03-20 17:39:06 +0800

    修改字段

--- src/main/java/com/jgw/supercodeplatform/marketing/controller/h5/OrganizationPortraitFrontController.java
+++ src/main/java/com/jgw/supercodeplatform/marketing/controller/h5/OrganizationPortraitFrontController.java
@@ -3,12 +3,16 @@ package com.jgw.supercodeplatform.marketing.controller.h5;
 
 import java.util.List;
 
+import org.apache.commons.lang.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.jgw.supercodeplatform.exception.SuperCodeException;
+import com.jgw.supercodeplatform.marketing.cache.GlobalRamCache;
 import com.jgw.supercodeplatform.marketing.common.model.RestResult;
+import com.jgw.supercodeplatform.marketing.common.model.activity.ScanCodeInfoMO;
 import com.jgw.supercodeplatform.marketing.common.util.CommonUtil;
 import com.jgw.supercodeplatform.marketing.dto.members.MarketingOrganizationPortraitListParam;
 import com.jgw.supercodeplatform.marketing.service.user.OrganizationPortraitService;
@@ -29,18 +33,35 @@ public class OrganizationPortraitFrontController  extends CommonUtil {
     @RequestMapping(value = "/getSelectedPor", method = RequestMethod.GET)
     @ApiOperation(value = "获取组织已选的画像编码", notes = "返回编码信息列表")
     @ApiImplicitParams({
-            @ApiImplicitParam(name = "organizationId", paramType = "query", defaultValue = "64b379cd47c843458378f479a115c322", value = "组织id", required = true)
+            @ApiImplicitParam(name = "wxstate", paramType = "query", defaultValue = "64b379cd47c843458378f479a115c322", value = "组织id", required = true)
     })
-    public RestResult<List<MarketingOrganizationPortraitListParam>> getSelectedPortrait(String organizationId) throws Exception {
+    public RestResult<List<MarketingOrganizationPortraitListParam>> getSelectedPortrait(String wxstate) throws Exception {
+		ScanCodeInfoMO scInfoMO=GlobalRamCache.scanCodeInfoMap.get(wxstate);
+		if (null==scInfoMO) {
+			throw new SuperCodeException("授权回调方法无法根据state="+wxstate+"获取到用户扫码缓存信息请重试", 500);
+		}
+		String organizationId=scInfoMO.getOrganizationId();
+		if (StringUtils.isBlank(organizationId)) {
+			throw new SuperCodeException("组织id不存在", 500);
+		}
+		
         return new RestResult<List<MarketingOrganizationPortraitListParam>>(200, "success", organizationPortraitService.getSelectedPortrait(organizationId));
     }
 
     @RequestMapping(value = "/getUnselectedPor", method = RequestMethod.GET)
     @ApiOperation(value = "获取组织未选的画像编码", notes = "返回编码信息列表")
     @ApiImplicitParams({
-    	   @ApiImplicitParam(name = "organizationId", paramType = "query", defaultValue = "64b379cd47c843458378f479a115c322", value = "组织id", required = true)
+    	   @ApiImplicitParam(name = "wxstate", paramType = "query", defaultValue = "64b379cd47c843458378f479a115c322", value = "组织id", required = true)
     })
-    public RestResult<String> getUnselectedPortrait(String organizationId) throws Exception {
+    public RestResult<String> getUnselectedPortrait(String wxstate) throws Exception {
+		ScanCodeInfoMO scInfoMO=GlobalRamCache.scanCodeInfoMap.get(wxstate);
+		if (null==scInfoMO) {
+			throw new SuperCodeException("授权回调方法无法根据state="+wxstate+"获取到用户扫码缓存信息请重试", 500);
+		}
+		String organizationId=scInfoMO.getOrganizationId();
+		if (StringUtils.isBlank(organizationId)) {
+			throw new SuperCodeException("组织id不存在", 500);
+		}
         return new RestResult(200, "success", organizationPortraitService.getUnselectedPortrait(organizationId));
     }
 
