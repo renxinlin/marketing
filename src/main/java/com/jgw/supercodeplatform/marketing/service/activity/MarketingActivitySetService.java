b647329f981de322bba4fe6f88297c9c29378174
zhuchuang <zhuchuang@app315.net>
2019-03-28 16:39:44 +0800

    mq

--- src/main/java/com/jgw/supercodeplatform/marketing/service/activity/MarketingActivitySetService.java
+++ src/main/java/com/jgw/supercodeplatform/marketing/service/activity/MarketingActivitySetService.java
@@ -723,6 +723,7 @@ public class MarketingActivitySetService  {
 				Object productBatchId=map.get("productBatchId");
 				Object codeTotal=map.get("codeTotal");
 				Object codeBatch=map.get("codeBatch");
+				logger.info("收到mq:productId="+productId+",productBatchId="+productBatchId+",codeTotal="+codeTotal+",codeBatch="+codeBatch);
 				if (null==productId || null==productBatchId ||null==codeTotal|| null==codeBatch) {
 					logger.error("获取码管理平台推送的新增批次mq消息，值有空值productId="+productId+",productBatchId="+productBatchId+",codeTotal="+codeTotal+",codeBatch="+codeBatch);
 					continue;
