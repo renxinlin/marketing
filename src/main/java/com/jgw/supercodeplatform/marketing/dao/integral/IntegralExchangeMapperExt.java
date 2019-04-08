a18a6334352fd5f57d6d5321ac23691eb33a584c
renxinlin <renxinlin@app315.net>
2019-04-08 16:28:22 +0800

    业务dao位置移动

--- src/main/java/com/jgw/supercodeplatform/marketing/dao/integral/IntegralExchangeMapperExt.java
+++ src/main/java/com/jgw/supercodeplatform/marketing/dao/integral/IntegralExchangeMapperExt.java
@@ -1,4 +1,4 @@
-package com.jgw.supercodeplatform.marketing.dao.integral.generator;
+package com.jgw.supercodeplatform.marketing.dao.integral;
 
 import com.jgw.supercodeplatform.marketing.dao.integral.generator.mapper.IntegralExchangeMapper;
 import org.apache.ibatis.annotations.Mapper;
