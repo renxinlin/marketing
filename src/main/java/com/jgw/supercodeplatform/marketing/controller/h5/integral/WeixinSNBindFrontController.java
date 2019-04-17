5b23233de580017615c54355f043d4cd404b5db7
zhuchuang <zhuchuang@app315.net>
2019-04-17 15:15:35 +0800

    授权地址

--- src/main/java/com/jgw/supercodeplatform/marketing/controller/h5/integral/WeixinSNBindFrontController.java
+++ src/main/java/com/jgw/supercodeplatform/marketing/controller/h5/integral/WeixinSNBindFrontController.java
@@ -1,4 +1,8 @@
-package com.jgw.supercodeplatform.marketing.controller.h5.activity;
+package com.jgw.supercodeplatform.marketing.controller.h5.integral;
+
+import java.net.URLEncoder;
+import java.util.HashMap;
+import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
@@ -22,24 +26,30 @@ import io.swagger.annotations.ApiOperation;
  */
 @RestController
 @RequestMapping("/marketing/front/snBind")
-@Api(tags = "获取企业公众号信息")
+@Api(tags = "获取授权微信商户信息及授权地址")
 public class WeixinSNBindFrontController extends CommonUtil {
     
     @Autowired
     private MarketingWxMerchantsService marketingWxMerchantsService;
-
+    
+    @Value("${marketing.domain.url}")
+    private String wxauthRedirectUri;
+    
     @RequestMapping(value = "/get",method = RequestMethod.GET)
-    @ApiOperation(value = "获取微信商户信息", notes = "")
+    @ApiOperation(value = "获取授权微信商户信息及授权地址", notes = "")
     @ApiImplicitParam(name = "organizationId", paramType = "query", defaultValue = "64b379cd47c843458378f479a115c322", value = "组织id", required = true)
-    public RestResult<MarketingWxMerchants> get(String organizationId) throws Exception {
+    public RestResult<Map<String, String>> get(String organizationId) throws Exception {
     	MarketingWxMerchants mWxMerchants=marketingWxMerchantsService.selectByOrganizationId(organizationId);
-    	RestResult<MarketingWxMerchants> restResult=new RestResult<MarketingWxMerchants>();
+    	RestResult<Map<String, String>> restResult=new RestResult<Map<String, String>>();
     	restResult.setState(200);
+    	Map<String, String> data=new HashMap<String, String>();
     	if (null!=mWxMerchants) {
-    		mWxMerchants.setMerchantSecret(null);
-    		mWxMerchants.setMerchantKey(null);
-    		mWxMerchants.setCertificatePassword(null);
-    		restResult.setResults(mWxMerchants);
+    		data.put("appId", mWxMerchants.getMchAppid());
+        	//微信授权需要对redirect_uri进行urlencode
+            String wholeUrl=wxauthRedirectUri+"/marketing/front/auth/code";
+        	String encoderedirectUri=URLEncoder.encode(wholeUrl, "utf-8");
+    		data.put("redriectUrl", encoderedirectUri);
+    		restResult.setResults(data);
 		}
         return restResult;
     }
