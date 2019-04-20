de0f8470d06a8d843ecf2cf6fa21825a62060b14
renxinlin <renxinlin@app315.net>
2019-04-20 15:18:11 +0800

    H5接口增加【订单记录，会员相关】，相关兑换动作相关逻辑，修改细节，修改订单表结构，缺少skuid[同时加了产品图片和售价处理]

---
+++ src/main/java/com/jgw/supercodeplatform/marketing/controller/h5/member/MemberContoller.java
@@ -0,0 +1,54 @@
+package com.jgw.supercodeplatform.marketing.controller.h5.member;
+
+import com.jgw.supercodeplatform.marketing.common.model.RestResult;
+import com.jgw.supercodeplatform.marketing.pojo.MarketingMembers;
+import com.jgw.supercodeplatform.marketing.pojo.integral.DeliveryAddress;
+import com.jgw.supercodeplatform.marketing.service.user.MarketingMembersService;
+import com.jgw.supercodeplatform.marketing.vo.activity.H5LoginVO;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiImplicitParams;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import springfox.documentation.annotations.ApiIgnore;
+
+@RestController
+@RequestMapping("/marketing/h5/member")
+@Api(tags = "H5会员信息管理")
+public class MemberContoller {
+
+
+    @Autowired
+    private MarketingMembersService service;
+
+
+    @RequestMapping(value = "/getMemberId",method = RequestMethod.GET)
+    @ApiOperation(value = "获取会员详情|获取会员积分", notes = "")
+    @ApiImplicitParams(value= {  @ApiImplicitParam(name = "jwt-token", paramType = "header", defaultValue = "ldpfbsujjknla;s.lasufuafpioquw949gyobrljaugf89iweubjkrlnkqsufi.awi2f7ygihuoquiu", value = "jwt-token信息", required = true)
+    })
+    public RestResult get(@ApiIgnore H5LoginVO jwtUser) throws Exception {
+        MarketingMembers memberById = service.getMemberById(jwtUser.getMemberId());
+        return RestResult.success("success", memberById);
+
+    }
+
+
+    @RequestMapping(value = "/update",method = RequestMethod.POST)
+    @ApiOperation(value = "获取会员详情|获取会员积分", notes = "")
+    @ApiImplicitParams(value= {  @ApiImplicitParam(name = "jwt-token", paramType = "header", defaultValue = "ldpfbsujjknla;s.lasufuafpioquw949gyobrljaugf89iweubjkrlnkqsufi.awi2f7ygihuoquiu", value = "jwt-token信息", required = true)
+    })
+    public RestResult update(@ApiIgnore MarketingMembers member, @ApiIgnore H5LoginVO jwtUser ) throws Exception {
+        // 通过jwt-token + H5LoginVO保证接口安全
+        service.update(member);
+        return RestResult.success("success",null);
+
+    }
+
+
+
+
+}
