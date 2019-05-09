d36a96e3ea275cbfb381b8fe4dcb8da5fe3ad055
renxinlin <renxinlin@app315.net>
2019-05-09 10:55:25 +0800

    info:提交枚举和vo

---
+++ src/main/java/com/jgw/supercodeplatform/marketing/diagram/vo/CricleVo.java
@@ -0,0 +1,18 @@
+package com.jgw.supercodeplatform.marketing.diagram.vo;
+
+import lombok.Data;
+
+/**
+ * 圆饼图
+ *
+ */
+@Data
+public class CricleVo {
+    //
+    private  String item;
+    private  double percent;
+    private  String percentStr;
+    private  int count;
+
+
+}
---
+++ src/main/java/com/jgw/supercodeplatform/marketing/diagram/vo/StatisicsVo.java
@@ -0,0 +1,19 @@
+package com.jgw.supercodeplatform.marketing.diagram.vo;
+
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+import lombok.Data;
+
+@ApiModel
+@Data
+public class StatisicsVo {
+    @ApiModelProperty("活动点击量")
+    private Integer clickNum;
+    @ApiModelProperty("微信红包发放累计金额")
+    private Integer moneyNum;
+    @ApiModelProperty("积分发放累计数值")
+    private Integer integralNum;
+    @ApiModelProperty("积分兑换累计数值")
+    private Integer exchangeNum;
+
+}
