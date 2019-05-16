2c5febd7c57174a748b8cf7737b782c6410cf5b1
renxinlin <renxinlin@app315.net>
2019-05-16 13:41:59 +0800

    info:门槛统一处理

--- src/main/java/com/jgw/supercodeplatform/marketing/pojo/MarketingActivitySetCondition.java
+++ src/main/java/com/jgw/supercodeplatform/marketing/pojo/MarketingActivitySetCondition.java
@@ -5,15 +5,30 @@ import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 import org.modelmapper.ModelMapper;
 
-/**
- * 活动门槛
- */
 @Data
 public class MarketingActivitySetCondition {
+
+
+    /**
+     * 活动门槛: 使用方式:
+     * 每次产品迭代出新的门槛条件
+     * 在这里加字段，以及字段使用方式说明
+     */
+
     @ApiModelProperty(name = "eachDayNumber", value = "每人每天最多参与", example = "1")
     private Integer eachDayNumber;
+    /**
+     *  定义统一枚举
+     *  0 无条件
+     *  1 协助领红包
+     *  2 协助领积分
+     */
     @ApiModelProperty(name = "participationCondition", value = "参与红包条件", example = "1")
     private Byte participationCondition;
+
+    /**
+     * 消耗积分数值
+     */
     @ApiModelProperty(name = "consumeIntegral", value = "消耗积分", example = "1")
     private Integer consumeIntegral;
     // 反序列化待定
@@ -51,8 +66,6 @@ public class MarketingActivitySetCondition {
     // 测试
     public static void main(String[] args) {
         MarketingActivitySetCondition object = new MarketingActivitySetCondition();
-        object.setConsumeIntegral(10);
-        object.setEachDayNumber(1);
          String s = JSONObject.toJSONString(object);
         System.out.println(s);
 
