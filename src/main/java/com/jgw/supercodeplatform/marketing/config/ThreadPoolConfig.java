50dc98fbd2e0a4ddd398490b8910760bb2912d1f
renxinlin <renxinlin@app315.net>
2019-05-20 17:58:40 +0800

    info:线程池调节

--- src/main/java/com/jgw/supercodeplatform/marketing/config/ThreadPoolConfig.java
+++ src/main/java/com/jgw/supercodeplatform/marketing/config/ThreadPoolConfig.java
@@ -16,9 +16,9 @@ public class ThreadPoolConfig {
 	@Bean(name="taskExecutor")
 	public TaskExecutor taskExecutor(){
 		ThreadPoolTaskExecutor executor=new ThreadPoolTaskExecutor();
-		executor.setCorePoolSize(700);//线程池维护线程最小数量,线程池的大小
-		executor.setMaxPoolSize(2000);//最大数量
-		executor.setQueueCapacity(700);//持有等待执行的任务队列,当当前线程数超过CorePoolSize就会等待
+		executor.setCorePoolSize(8);//线程池维护线程最小数量,线程池的大小
+		executor.setMaxPoolSize(16);//最大数量
+		executor.setQueueCapacity(500);//持有等待执行的任务队列,当当前线程数超过CorePoolSize就会等待
 		executor.setKeepAliveSeconds(5);//线程多久没执行关闭线程,单位秒,默认60秒
 		executor.setThreadNamePrefix("my_thread");//线程名前缀
 		executor.initialize();
