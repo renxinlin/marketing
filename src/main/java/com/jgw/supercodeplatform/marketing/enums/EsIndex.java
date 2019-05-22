2af78a150b012e74352d8b3f84bb3435edbdbb29
renxinlin <renxinlin@app315.net>
2019-05-22 18:41:07 +0800

    info:索引新增

--- src/main/java/com/jgw/supercodeplatform/marketing/enums/EsIndex.java
+++ src/main/java/com/jgw/supercodeplatform/marketing/enums/EsIndex.java
@@ -4,9 +4,9 @@ package com.jgw.supercodeplatform.marketing.enums;
  * @author Created by jgw136 on 2018/04/27.
  */
 public enum  EsIndex {
-    MARKETING("marketing"),INTEGRAL("integral"),MARKET_DIAGRAM_REMBER("marketdiagram");
+    MARKETING("marketing"),INTEGRAL("integral"),MARKET_DIAGRAM_REMBER("marketdiagram"),
 
-    ;
+    MARKET_SCAN_INFO("marketingscan");
 
     private String index;
 
