f8648ec4bb68ed9b1a5e5815aa1811526792d5d9
renxinlin <renxinlin@app315.net>
2019-06-11 16:13:00 +0800

    info: ext提交

--- src/main/java/com/jgw/supercodeplatform/marketing/dao/coupon/MarketingCouponMapperExt.java
+++ src/main/java/com/jgw/supercodeplatform/marketing/dao/coupon/MarketingCouponMapperExt.java
@@ -1,8 +1,30 @@
 package com.jgw.supercodeplatform.marketing.dao.coupon;
 
+import com.jgw.supercodeplatform.marketing.dao.CommonSql;
 import com.jgw.supercodeplatform.marketing.dao.integral.generator.mapper.MarketingCouponMapper;
+import com.jgw.supercodeplatform.marketing.pojo.MarketingChannel;
+import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
 
 @Mapper
-public interface MarketingCouponMapperExt extends MarketingCouponMapper{
+public interface MarketingCouponMapperExt extends MarketingCouponMapper, CommonSql {
+
+    @Insert(startScript +
+            " insert into marketing_coupon ( ActivitySetId, "+
+            " CouponAmount, DeductionDate, "+
+            " DeductionProductType, DeductionChannelType ) values "+
+            "<foreach collection='list' item='channel' index='index' separator=','>"+
+            "(" +
+            "#{channel.activitySetId}," +
+            "#{channel.couponAmount}," +
+            "#{channel.deductionDate}," +
+            "#{channel.deductionProductType}," +
+            "#{channel.deductionChannelType}" +
+            ")"+
+            "</foreach>"+
+            endScript)
+    int batchInsert(@Param("list") List<MarketingChannel> list);
 }
