41f95e636075c3f4ac56a3f97f3813afdbd702ff
renxinlin <renxinlin@app315.net>
2019-06-11 16:03:16 +0800

    info:提交一个优惠券规则ext

---
+++ src/main/java/com/jgw/supercodeplatform/marketing/dao/coupon/MarketingCouponMapperExt.java
@@ -0,0 +1,8 @@
+package com.jgw.supercodeplatform.marketing.dao.coupon;
+
+import com.jgw.supercodeplatform.marketing.dao.integral.generator.mapper.MarketingCouponMapper;
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface MarketingCouponMapperExt extends MarketingCouponMapper{
+}
