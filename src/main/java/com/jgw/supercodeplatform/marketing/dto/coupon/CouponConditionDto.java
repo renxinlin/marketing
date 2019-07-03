0d1d720c76ae29c84988aa9c42427005dcdb09af
liuyulin <liuyulin@app315.net>
2019-07-03 22:11:10 +0800

    核销

---
+++ src/main/java/com/jgw/supercodeplatform/marketing/dto/coupon/CouponConditionDto.java
@@ -0,0 +1,12 @@
+package com.jgw.supercodeplatform.marketing.dto.coupon;
+
+import lombok.Data;
+
+@Data
+public class CouponConditionDto {
+	
+	private Byte eductionProductType;
+	
+	private Byte deductionChannelType;
+
+}
