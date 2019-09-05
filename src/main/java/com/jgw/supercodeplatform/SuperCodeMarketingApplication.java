f1d8784bae383d539ac37d206282df4372c806a8
renxinlin <renxinlin@app315.net>
2019-09-05 16:08:04 +0800

    info:微服务模块开启

--- src/main/java/com/jgw/supercodeplatform/SuperCodeMarketingApplication.java
+++ src/main/java/com/jgw/supercodeplatform/SuperCodeMarketingApplication.java
@@ -4,6 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
@@ -16,6 +18,8 @@ import com.ctrip.framework.apollo.spring.annotation.EnableApolloConfig;
 @EnableScheduling
 @EnableTransactionManagement
 @EnableApolloConfig
+@EnableFeignClients
+@EnableHystrix
 public class SuperCodeMarketingApplication {
 
 
