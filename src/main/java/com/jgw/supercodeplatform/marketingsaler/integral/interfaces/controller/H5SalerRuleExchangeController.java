1e95a6c782ea66f0751b106aae174d81db53e2a2
renxinlin <renxinlin@app315.net>
2019-09-10 17:39:13 +0800

    bug修改

--- src/main/java/com/jgw/supercodeplatform/marketingsaler/integral/interfaces/controller/H5SalerRuleExchangeController.java
+++ src/main/java/com/jgw/supercodeplatform/marketingsaler/integral/interfaces/controller/H5SalerRuleExchangeController.java
@@ -59,7 +59,7 @@ public class H5SalerRuleExchangeController extends SalerCommonController {
     @PostMapping("/exchange")
     @ApiOperation(value = "兑换", notes = "")
     @ApiImplicitParam(name = "jwt-token", paramType = "header", defaultValue = "64b379cd47c843458378f479a115c322", value = "token信息", required = true)
-    public RestResult exchange(@Valid @RequestBody H5SalerRuleExchangeDto salerRuleExchangeDto, H5LoginVO user) throws CommonException {
+    public RestResult exchange(@Valid @RequestBody H5SalerRuleExchangeDto salerRuleExchangeDto,@ApiIgnore H5LoginVO user) throws CommonException {
         service.exchange(salerRuleExchangeDto,user);
         return success();
     }
