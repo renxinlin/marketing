c7ba698bb3b10e0dd1bbc148026b91d4d355c5ae
renxinlin <renxinlin@app315.net>
2019-09-17 17:56:04 +0800

    info:新增状态拦截

--- src/main/java/com/jgw/supercodeplatform/marketingsaler/order/service/SalerOrderFormService.java
+++ src/main/java/com/jgw/supercodeplatform/marketingsaler/order/service/SalerOrderFormService.java
@@ -255,6 +255,7 @@ public class SalerOrderFormService extends SalerCommonService<SalerOrderFormMapp
     public void saveOrder(List<ColumnnameAndValueDto> columnnameAndValues, H5LoginVO user) {
         Asserts.check(!StringUtils.isEmpty(user.getOrganizationId()), "未获取对应组织");
         Asserts.check(!CollectionUtils.isEmpty(columnnameAndValues), "未获取订货信息");
+        validAllHaveColumn(columnnameAndValues);
         StringBuffer address = new StringBuffer("");
         if (!StringUtils.isEmpty(user.getCustomerId())) {
             CustomerInfoView customerInfo = baseCustomerService.getCustomerInfo(user.getCustomerId());
@@ -263,6 +264,15 @@ public class SalerOrderFormService extends SalerCommonService<SalerOrderFormMapp
         }
         SalerOrderTransfer.initDefaultColumnValue(columnnameAndValues, user, address.toString());
         dynamicMapper.saveOrder(columnnameAndValues, SalerOrderTransfer.initTableName(user.getOrganizationId()));
+    }
+
+    private void validAllHaveColumn(List<ColumnnameAndValueDto> columnnameAndValues) {
+        int orderSize = baseMapper.selectCount(query().eq("organizationId", commonUtil.getOrganizationId()).getWrapper());
+        int size = orderSize-SalerOrderTransfer.deafultColumnNames.size();
+        if(columnnameAndValues.size() != size){
+            throw new RuntimeException("数据为必填...");
+        }
+
     }
 
     /**
