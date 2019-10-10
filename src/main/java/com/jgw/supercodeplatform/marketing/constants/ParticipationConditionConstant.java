9e3e592ae7182f9e0f6cf311dd7da1b7fe993667
liuyulin <liuyulin@app315.net>
2019-10-10 14:54:21 +0800

    orgId

--- src/main/java/com/jgw/supercodeplatform/marketing/constants/ParticipationConditionConstant.java
+++ src/main/java/com/jgw/supercodeplatform/marketing/constants/ParticipationConditionConstant.java
@@ -1,7 +1,7 @@
 package com.jgw.supercodeplatform.marketing.constants;
 
 public interface ParticipationConditionConstant {
-    int noCondition = 0;
-    int activity =1;
+    int noCondition = 1;
     int integral = 2;
+    int activity =3;
 }
