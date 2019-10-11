e3ecb580c62a25b59485a77773ed809403c2c3b3
renxinlin <renxinlin@app315.net>
2019-10-11 15:01:35 +0800

    info：接口文档

--- src/main/java/com/jgw/supercodeplatform/prizewheels/interfaces/WheelsController.java
+++ src/main/java/com/jgw/supercodeplatform/prizewheels/interfaces/WheelsController.java
@@ -2,22 +2,19 @@ package com.jgw.supercodeplatform.prizewheels.interfaces;
 
 
 import com.jgw.supercodeplatform.marketing.common.model.RestResult;
-import com.jgw.supercodeplatform.marketing.controller.common.CommonController;
-import com.jgw.supercodeplatform.marketing.vo.activity.H5LoginVO;
-import com.jgw.supercodeplatform.marketingsaler.base.config.aop.CheckRole;
+import com.jgw.supercodeplatform.marketing.common.page.AbstractPageService;
+import com.jgw.supercodeplatform.marketing.common.page.DaoSearch;
 import com.jgw.supercodeplatform.marketingsaler.base.controller.SalerCommonController;
-import com.jgw.supercodeplatform.marketingsaler.order.service.SalerOrderFormService;
 import com.jgw.supercodeplatform.prizewheels.application.service.WheelsPublishAppication;
-import com.jgw.supercodeplatform.prizewheels.interfaces.assembler.WheelsAssembler;
 import com.jgw.supercodeplatform.prizewheels.interfaces.dto.WheelsDto;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiImplicitParam;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.util.List;
 
 @RestController
 @RequestMapping("marketing/prizeWheels")
@@ -31,8 +28,41 @@ public class WheelsController extends SalerCommonController {
     @PostMapping("/add")
     @ApiOperation(value = "添加", notes = "")
     @ApiImplicitParam(name = "super-token", paramType = "header", defaultValue = "64b379cd47c843458378f479a115c322", value = "token信息", required = true)
-    public RestResult add(@RequestBody WheelsDto wheelsDto)   {
+    public RestResult add(@Valid  @RequestBody WheelsDto wheelsDto)   {
         appication.publish(wheelsDto);
+        return success();
+    }
+
+
+    @PostMapping("/update")
+    @ApiOperation(value = "更新", notes = "")
+    @ApiImplicitParam(name = "super-token", paramType = "header", defaultValue = "64b379cd47c843458378f479a115c322", value = "token信息", required = true)
+    public RestResult update(@Valid  @RequestBody WheelsDto wheelsDto)   {
+        // appication.publish(wheelsDto);
+        return success();
+    }
+
+
+
+    @GetMapping("/detail")
+    @ApiOperation(value = "详情", notes = "")
+    @ApiImplicitParam(name = "super-token", paramType = "header", defaultValue = "64b379cd47c843458378f479a115c322", value = "token信息", required = true)
+    public RestResult<WheelsDto> detail(@RequestParam("id") Integer id)   {
+        return success();
+    }
+
+
+    @GetMapping("/delete")
+    @ApiOperation(value = "删除", notes = "")
+    @ApiImplicitParam(name = "super-token", paramType = "header", defaultValue = "64b379cd47c843458378f479a115c322", value = "token信息", required = true)
+    public RestResult delete(@RequestParam("id") Integer id)   {
+        return success();
+    }
+
+    @GetMapping("/add")
+    @ApiOperation(value = "分页", notes = "")
+    @ApiImplicitParam(name = "super-token", paramType = "header", defaultValue = "64b379cd47c843458378f479a115c322", value = "token信息", required = true)
+    public RestResult<AbstractPageService.PageResults<List<WheelsDto>>> list(DaoSearch daoSearch)   {
         return success();
     }
 
