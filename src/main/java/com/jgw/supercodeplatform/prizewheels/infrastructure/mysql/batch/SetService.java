e1484f743377ebc99a0518c48fbfa9f74c5b2174
renxinlin <renxinlin@app315.net>
2019-10-14 11:39:29 +0800

    bug模板问题修改

--- src/main/java/com/jgw/supercodeplatform/prizewheels/infrastructure/mysql/batch/SetService.java
+++ src/main/java/com/jgw/supercodeplatform/prizewheels/infrastructure/mysql/batch/SetService.java
@@ -1,12 +1,10 @@
 package com.jgw.supercodeplatform.prizewheels.infrastructure.mysql.batch;
 
 
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.jgw.supercodeplatform.prizewheels.infrastructure.mysql.mapper.ActivitySetMapper;
 import com.jgw.supercodeplatform.prizewheels.infrastructure.mysql.pojo.ActivitySet;
 import org.springframework.stereotype.Service;
-
-
-import com.jgw.supercodeplatform.prizewheels.infrastructure.mysql.mapper.ActivitySetMapper;
-import   com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 
 /**
  * <p>
@@ -16,7 +14,7 @@ import   com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
  * @since 2019-10-14
  */
 @Service
-public class SetService extends ServiceImpl<ActivitySetMapper<ActivitySet>, ActivitySet> {
+public class SetService extends ServiceImpl<ActivitySetMapper, ActivitySet> {
 
 
 }
