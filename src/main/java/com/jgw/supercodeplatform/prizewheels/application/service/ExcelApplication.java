93dcade726901b5cada2a8fd746bbd8db82a0b07
fangshiping <fangshiping@app315.net>
2019-10-14 17:00:15 +0800

    info:fsp excel

--- src/main/java/com/jgw/supercodeplatform/prizewheels/application/service/ExcelApplication.java
+++ src/main/java/com/jgw/supercodeplatform/prizewheels/application/service/ExcelApplication.java
@@ -3,10 +3,10 @@ package com.jgw.supercodeplatform.prizewheels.application.service;
 import com.jgw.supercodeplatform.marketing.common.util.CommonUtil;
 import com.jgw.supercodeplatform.marketing.common.util.ExcelUtils;
 import com.jgw.supercodeplatform.marketing.exception.base.ExcelException;
+import com.jgw.supercodeplatform.prizewheels.infrastructure.mysql.batch.WheelsRewardCdkService;
 import com.jgw.supercodeplatform.prizewheels.infrastructure.mysql.pojo.WheelsRewardCdk;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.springframework.web.multipart.MultipartFile;
 
 import java.io.InputStream;
 import java.util.ArrayList;
@@ -22,24 +22,35 @@ public class ExcelApplication {
     @Autowired
     private CommonUtil commonUtil;
 
+    @Autowired
+    private WheelsRewardCdkService wheelsRewardCdkService;
+
     private static final String sheetName = "Sheet1";
     /**
      * 上传奖励报表excel
      * @param is
      */
-    public void uploadExcel(InputStream is) throws ExcelException {
+    public String uploadExcel(InputStream is) throws ExcelException {
         LinkedHashMap linkedHashMap = new LinkedHashMap();
         linkedHashMap.put("prize_wheels_reward_cdk","cdk");
         String[] uniqueFields={"奖励列表"};
         List<WheelsRewardCdk> wheelsRewardCdks=ExcelUtils.excelToList(is,sheetName,WheelsRewardCdk.class,linkedHashMap,uniqueFields);
-        List<WheelsRewardCdk> uploadwheelsRewardCdks=new ArrayList<>(wheelsRewardCdks.size()-1);
+        //提取有用的cdk
+        List<WheelsRewardCdk> exitWheelsRewardCdks=new ArrayList<>(wheelsRewardCdks.size()-1);
 
-        for (WheelsRewardCdk wheelsRewardCdk:wheelsRewardCdks) {
+        for(int i=1;i<wheelsRewardCdks.size();i++){
+            exitWheelsRewardCdks.add(wheelsRewardCdks.get(i));
+        }
+        //插入数据库的list对象
+        List<WheelsRewardCdk> uploadwheelsRewardCdks=new ArrayList<>(exitWheelsRewardCdks.size());
+        String UUID=commonUtil.getUUID();
+        for (WheelsRewardCdk wheelsRewardCdk:exitWheelsRewardCdks) {
             WheelsRewardCdk wheelsRewardCdk1=new WheelsRewardCdk();
             wheelsRewardCdk1.setCdk(wheelsRewardCdk.getCdk());
-            wheelsRewardCdk1.setCdkKey(commonUtil.getUUID());
+            wheelsRewardCdk1.setCdkKey(UUID);
             uploadwheelsRewardCdks.add(wheelsRewardCdk1);
         }
-        
+        wheelsRewardCdkService.saveBatch(uploadwheelsRewardCdks);
+        return UUID;
     }
 }
