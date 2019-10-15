2f58861d980618288248824678fe78a8b3f64381
renxinlin <renxinlin@app315.net>
2019-10-15 09:35:27 +0800

    info：dev ing

--- src/main/java/com/jgw/supercodeplatform/prizewheels/interfaces/dto/PrizeWheelsRewardDto.java
+++ src/main/java/com/jgw/supercodeplatform/prizewheels/interfaces/dto/PrizeWheelsRewardDto.java
@@ -1,6 +1,7 @@
 package com.jgw.supercodeplatform.prizewheels.interfaces.dto;
 
 import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.experimental.Accessors;
@@ -10,11 +11,12 @@ import lombok.experimental.Accessors;
 @Accessors(chain = true)
 @ApiModel("H5领奖dto")
 public class PrizeWheelsRewardDto {
+    @ApiModelProperty("大转盘活动id")
     private Long id;
 
-
+    @ApiModelProperty("外码")
     private String outerCodeId;
 
-
+    @ApiModelProperty("码制")
     private String codeTypeId;
 }
