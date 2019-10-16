cd47044381f0b9f8c3fbd117b8155f1a4209f008
renxinlin <renxinlin@app315.net>
2019-10-16 16:19:18 +0800

    bug:未中奖名称

--- src/main/java/com/jgw/supercodeplatform/prizewheels/infrastructure/domainserviceimpl/ProductDomainServiceImpl.java
+++ src/main/java/com/jgw/supercodeplatform/prizewheels/infrastructure/domainserviceimpl/ProductDomainServiceImpl.java
@@ -36,6 +36,7 @@ public class ProductDomainServiceImpl implements ProductDomainService {
         List<GetBatchInfoDto> getBatchInfoDtoList =  productDomainTranfer.tranferProductsToGetBatchInfoDtos(products);
         // 两个限界上下文交互
         RestResult<Object> sbatchIds = getSbatchIdsByPrizeWheelsFeign.getSbatchIds(getBatchInfoDtoList);
+        log.info(" initSbatchIds请求返回如下",JSONObject.toJSONString(sbatchIds));
 
         // 业务
         if(!CollectionUtils.isEmpty((ArrayList)sbatchIds.getResults()) && sbatchIds.getState() ==200 ){
@@ -52,7 +53,7 @@ public class ProductDomainServiceImpl implements ProductDomainService {
             });
 
         }else {
-            log.info("initSbatchIds(List<Product> products) =》 {}",JSONObject.toJSONString(products));
+            log.info("initSbatchIds(List<Product> products) =》 {}",JSONObject.toJSONString(getBatchInfoDtoList));
             throw new RuntimeException("获取码管理生码信息失败");
         }
         // 携带生码批次
