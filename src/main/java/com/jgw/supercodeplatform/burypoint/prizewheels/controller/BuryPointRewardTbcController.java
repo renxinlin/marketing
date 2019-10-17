896013459c81709834577122bd6de808b5f64f61
fangshiping <fangshiping@app315.net>
2019-10-17 10:35:48 +0800

    info:埋点剔除验证

--- src/main/java/com/jgw/supercodeplatform/burypoint/prizewheels/controller/BuryPointRewardTbcController.java
+++ src/main/java/com/jgw/supercodeplatform/burypoint/prizewheels/controller/BuryPointRewardTbcController.java
@@ -1,8 +1,6 @@
 package com.jgw.supercodeplatform.burypoint.prizewheels.controller;
 
-import com.jgw.supercodeplatform.burypoint.prizewheels.dto.outerchain.BuryPointOuterChainTcDto;
 import com.jgw.supercodeplatform.burypoint.prizewheels.dto.reward.BuryPointRewardTbcDto;
-import com.jgw.supercodeplatform.burypoint.prizewheels.service.BuryPointOuterChainTcService;
 import com.jgw.supercodeplatform.burypoint.prizewheels.service.BuryPointRewardTbcService;
 import com.jgw.supercodeplatform.marketing.common.model.RestResult;
 import com.jgw.supercodeplatform.marketingsaler.base.controller.SalerCommonController;
@@ -15,28 +13,17 @@ import javax.validation.Valid;
 
 /**
  * @author fangshiping
- * @date 2019/10/16 13:06
+ * @date 2019/10/16 14:10
  */
 @RestController
 @RequestMapping("marketing/prizeWheels/buryPoint")
-@Api(tags = "大转盘埋点")
-public class BuryPointController extends SalerCommonController {
-
-    @Autowired
-    private BuryPointOuterChainTcService buryPointOuterChainTcService;
-
+@Api(tags = "大转盘发放奖励埋点")
+public class BuryPointRewardTbcController extends SalerCommonController {
     @Autowired
     private BuryPointRewardTbcService buryPointRewardTbcService;
 
-    @PostMapping(value = "/addOuterChain")
-    @ApiOperation(value = "插入C端点击外链埋点数据")
-    public RestResult buryPointOuterChainTc(@RequestBody BuryPointOuterChainTcDto buryPointOuterChainTcDto){
-        buryPointOuterChainTcService.buryPointOuterChainTc(buryPointOuterChainTcDto);
-        return success();
-    }
-
-    @PostMapping(value = "/addReward")
-    @ApiOperation(value = "插入C端领取奖励埋点数据")
+    @PostMapping("/addReward")
+    @ApiOperation(value = "插入b端领取奖励埋点数据")
     public RestResult buryPointRewardTbc(@RequestBody BuryPointRewardTbcDto buryPointRewardTbcDto){
         buryPointRewardTbcService.buryPointRewardTbc(buryPointRewardTbcDto);
         return success();
--- src/main/java/com/jgw/supercodeplatform/burypoint/prizewheels/dto/reward/BuryPointRewardTbcDto.java
+++ src/main/java/com/jgw/supercodeplatform/burypoint/prizewheels/dto/reward/BuryPointRewardTbcDto.java
@@ -14,12 +14,9 @@ public class BuryPointRewardTbcDto {
     /**
      * 奖励id
      */
-    @NotBlank(message = "奖励id不可为空")
     private String rewardId;
 
-    @NotBlank(message = "奖励name不可为空")
     private String rewardName;
 
-    @NotBlank(message = "活动id不可为空")
     private String activityId;
 }
