e238c75746d4f8b081eeb2bf96fdbfd720f39637
renxinlin <renxinlin@app315.net>
2019-10-22 15:01:53 +0800

    warn： 新增组织信息

--- src/main/java/com/jgw/supercodeplatform/prizewheels/interfaces/H5WheelsController.java
+++ src/main/java/com/jgw/supercodeplatform/prizewheels/interfaces/H5WheelsController.java
@@ -6,13 +6,10 @@ import com.jgw.supercodeplatform.marketingsaler.base.config.aop.CheckRole;
 import com.jgw.supercodeplatform.marketingsaler.base.controller.SalerCommonController;
 import com.jgw.supercodeplatform.marketingsaler.base.exception.CommonException;
 import com.jgw.supercodeplatform.marketingsaler.common.Role;
-import com.jgw.supercodeplatform.marketingsaler.integral.interfaces.controller.H5SalerRuleExchangeController;
-import com.jgw.supercodeplatform.marketingsaler.integral.interfaces.dto.H5SalerRuleExchangeDto;
 import com.jgw.supercodeplatform.prizewheels.application.service.GetWheelsRewardApplication;
 import com.jgw.supercodeplatform.prizewheels.domain.constants.CallBackConstant;
 import com.jgw.supercodeplatform.prizewheels.domain.model.WheelsRewardCdk;
 import com.jgw.supercodeplatform.prizewheels.interfaces.dto.PrizeWheelsRewardDto;
-import com.jgw.supercodeplatform.prizewheels.interfaces.dto.WheelsRewardDto;
 import com.jgw.supercodeplatform.prizewheels.interfaces.vo.WheelsDetailsVo;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiImplicitParam;
@@ -24,6 +21,7 @@ import org.springframework.web.bind.annotation.*;
 import springfox.documentation.annotations.ApiIgnore;
 
 import javax.validation.Valid;
+
 @Slf4j
 @Controller
 @RequestMapping("marketing/front/prizeWheels")
@@ -32,6 +30,7 @@ public class H5WheelsController extends SalerCommonController {
 
     @Autowired
     private GetWheelsRewardApplication application;
+
     @CheckRole(role = Role.vip)
     @ResponseBody
     @PostMapping("/reward")
@@ -39,22 +38,23 @@ public class H5WheelsController extends SalerCommonController {
     @ApiImplicitParam(name = "jwt-token", paramType = "header", defaultValue = "64b379cd47c843458378f479a115c322", value = "token信息", required = true)
     public RestResult<WheelsRewardCdk> reward(@Valid @RequestBody PrizeWheelsRewardDto prizeWheelsRewardDto, @ApiIgnore H5LoginVO user) throws CommonException {
         // 暂时只有虚拟wu
-        return success(application.reward(prizeWheelsRewardDto,user));
+        return success(application.reward(prizeWheelsRewardDto, user));
     }
 
 
     @GetMapping("/redrict")
     @ApiOperation(value = "大转盘码管理重定向", notes = "")
-    public String reward(String productBatchId, String outerCodeId, String codeTypeId,String organizationId)   {
-        // 扫码重定向到前端 基于产品批次找到活动 TODO 码管理直接重定向到前端
-        String url = "redirect:"
-                + CallBackConstant.TO_WEB_URL
-                + "?productBatchId="+productBatchId
-                + "&outerCodeId="+outerCodeId
-                + "&codeTypeId="+codeTypeId
-                + "&template=10"  // 业务标志字段
-                 + "&organizationId="+organizationId ; //  组织字段
-        log.info("==> 大转盘扫码重定向到前端{} ",url);
+    public String reward(String productBatchId, String outerCodeId, String codeTypeId, String organizationId) {
+        // 扫码重定向到前端 基于产品批次找到活动
+        StringBuffer urlBuffe = new StringBuffer("redirect:");
+        urlBuffe.append(CallBackConstant.TO_WEB_URL)
+                .append("?productBatchId=").append(productBatchId)
+                .append("&outerCodeId=").append(outerCodeId)
+                .append("&codeTypeId=").append(codeTypeId)
+                .append("&template=10") // 业务标志字段
+                .append("&organizationId=").append(organizationId);
+        String url = urlBuffe.toString();
+        log.info("==> 大转盘扫码重定向到前端{} ", url);
         return url;
     }
 
@@ -63,13 +63,10 @@ public class H5WheelsController extends SalerCommonController {
     @GetMapping("/detail")
     @ApiOperation(value = "H5大转盘详情", notes = "")
     @ApiImplicitParam(name = "jwt-token", paramType = "header", defaultValue = "64b379cd47c843458378f479a115c322", value = "token信息", required = true)
-    public RestResult detail(@RequestParam String productBatchId){
+    public RestResult detail(@RequestParam String productBatchId) {
         WheelsDetailsVo wheelsDetailsVO = application.detail(productBatchId);
         return success(wheelsDetailsVO);
     }
-
-
-
 
 
 }
