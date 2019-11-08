d4fba47c90e61dedca7d2536a36454e3e9499f38
renxinlin <jgw123456>
2019-11-08 11:37:57 +0800

    info:大转盘金额

---
+++ src/main/java/com/jgw/supercodeplatform/prizewheels/domain/model/MoneyCalculator.java
@@ -0,0 +1,8 @@
+package com.jgw.supercodeplatform.prizewheels.domain.model;
+
+public class MoneyCalculator {
+
+    public double buildRewardMoney(WheelsReward wheelsReward){
+        return 0D;
+    }
+}
