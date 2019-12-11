a68f20665ff479451e8cc7e2570c940271faa242
renxinlin <jgw123456>
2019-12-11 18:14:10 +0800

    bug:批次

--- src/main/java/com/jgw/supercodeplatform/marketing/common/constants/SexConstants.java
+++ src/main/java/com/jgw/supercodeplatform/marketing/common/constants/SexConstants.java
@@ -5,7 +5,7 @@ package com.jgw.supercodeplatform.marketing.common.constants;
  * @date 2019/11/11 16:02
  */
 public class SexConstants {
-    public static final Byte WOMEN=0;
-    public static final Byte MEN=1;
+    public static final Byte WOMEN=1;
+    public static final Byte MEN=0;
     public static final String SEX="sex";
 }
